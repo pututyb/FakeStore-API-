ae354e0ad6e25c517475c617c6f14272856ccf38
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-07-26 07:23:36 +0700

    Initial Commit

---
+++ FakeStore(API)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeStore(API)
+//
+//  Created by Putut Yusri Bahtiar on 26/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
