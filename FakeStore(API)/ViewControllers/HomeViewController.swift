e1f9b68abfbe15302d6444617ba6b81cf6e1edc1
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-07-28 14:06:03 +0700

    let

--- FakeStore(API)/ViewControllers/HomeViewController.swift
+++ FakeStore(API)/ViewControllers/HomeViewController.swift
@@ -13,9 +13,18 @@ class HomeViewController: UIViewController {
         let btnBurgerMenu = UIButton()
         btnBurgerMenu.translatesAutoresizingMaskIntoConstraints = false
         btnBurgerMenu.backgroundColor = UIColor(named: "btnBurgerMenu")
-        btnBurgerMenu.layer.cornerRadius = 20
+        btnBurgerMenu.layer.cornerRadius = 18
         btnBurgerMenu.tintColor = UIColor(named: "textPrimaryColor")
         return btnBurgerMenu
+    }()
+    
+    private lazy var btnBagMenu: UIButton = {
+        let btnBagMenu = UIButton()
+        btnBagMenu.translatesAutoresizingMaskIntoConstraints = false
+        btnBagMenu.backgroundColor = UIColor(named: "btnBurgerMenu")
+        btnBagMenu.layer.cornerRadius = 18
+        btnBagMenu.tintColor = UIColor(named: "textPrimaryColor")
+        return btnBagMenu
     }()
     
     override func viewDidLoad() {
@@ -24,13 +33,19 @@ class HomeViewController: UIViewController {
         view.backgroundColor = UIColor(named: "appBackgroundColor")
         
         view.addSubview(btnBurgerMenu)
+        view.addSubview(btnBagMenu)
         
         
         NSLayoutConstraint.activate([
             btnBurgerMenu.topAnchor.constraint(equalTo: view.topAnchor, constant: 55),
             btnBurgerMenu.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            btnBurgerMenu.widthAnchor.constraint(equalToConstant: 45),
-            btnBurgerMenu.heightAnchor.constraint(equalToConstant: 45),
+            btnBurgerMenu.widthAnchor.constraint(equalToConstant: 50),
+            btnBurgerMenu.heightAnchor.constraint(equalToConstant: 50),
+            
+            btnBagMenu.topAnchor.constraint(equalTo: view.topAnchor, constant: 55),
+            btnBagMenu.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            btnBagMenu.widthAnchor.constraint(equalToConstant: 50),
+            btnBagMenu.heightAnchor.constraint(equalToConstant: 50),
         ])
         
         updateBurgerButtonImage()
@@ -47,8 +62,10 @@ class HomeViewController: UIViewController {
     private func updateBurgerButtonImage() {
         if traitCollection.userInterfaceStyle == .dark {
             btnBurgerMenu.setImage(UIImage(named: "burgerLight"), for: .normal)
+            btnBagMenu.setImage(UIImage(named: "bagLight"), for: .normal)
         } else {
             btnBurgerMenu.setImage(UIImage(named: "burgerDark"), for: .normal)
+            btnBagMenu.setImage(UIImage(named: "bagDark"), for: .normal)
         }
     }
 }
